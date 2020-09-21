1bcec3806338fab0ed2cd067e3f181c8e8788573
alitaee <ali.taee.72@gmail.com>
2020-09-21 22:35:44 +0330

    Add watch band and change styles to scss

--- src/Banner.jsx
+++ src/Banner.jsx
@@ -1,8 +1,19 @@
 import React from "react";
-import "./styles/main.css";
+import "./styles/main.scss";
 
 function Banner() {
-  return <section className="center">React HTML Banner</section>;
+  return (
+    <section className="center">
+      <div className="bnr">
+        <div className="bnr__el bnr__el-0 ">
+          <div className="bnr__band bnr__el-bg"></div>
+        </div>
+        <div className="bnr__el bnr__el-1 ">
+          <div className="bnr__watch bnr__el-bg"></div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Banner;
