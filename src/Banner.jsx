a748e639676369d5098ef1e9d909316a639bb69f
alitaee <ali.taee.72@gmail.com>
2020-09-21 08:33:09 +0330

    Delete init files and create basic folder structure

---
+++ src/Banner.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./styles/main.css";
+
+function Banner() {
+  return <section className="center">React HTML Banner</section>;
+}
+
+export default Banner;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import Banner from "./Banner.jsx";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Banner />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
