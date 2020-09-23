5d92d84fd73599434d6cfe12a2a0e146e3b4f68a
alitaee <ali.taee.72@gmail.com>
2020-09-24 00:14:55 +0330

    Add config file for apple watch banner

--- src/banners/apple-watch/banner.jsx
+++ src/banners/apple-watch/banner.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./banner.scss";
+import Config from './config.js'
 
 const AppleWatchBanner = () => {
   return (
@@ -11,13 +12,13 @@ const AppleWatchBanner = () => {
         <div className="bnrw__watch bnrw__el-bg"></div>
       </div>
       <div className="bnrw__el-2">
-        <a className="bnrw__btn" href="https://www.apple.com/watch/">
-          buy
+        <a className="bnrw__btn" href={Config.link}>
+          {Config.btn}
         </a>
       </div>
       <div className="bnrw__el-3">
         <span className="bnrw__title">
-          apple watch <span className="bnrw__title-red">series 6</span>
+        {Config.brand} <span className="bnrw__title-red">{Config.model}</span>
         </span>
       </div>
     </div>
